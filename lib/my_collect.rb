2492f2aa4c1f1f28a758c71c0e6027c50f28108c
Mark Baruch <mbaruch28@gmail.com>
2019-02-13 20:27:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collection (array)
   i = 0
-  new_collect = [ ]
+  new_collect = []
 
-  yield i < collect.length
+  while i < collect.length
    new_collect << yield(array[i])
     i += 1
   end
